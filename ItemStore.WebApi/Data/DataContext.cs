32c29a828617c2a0788bed723f64d91fa86a625a
JauniusPinelis <JauniusPinelis@gmail.com>
2021-04-28 14:09:24 +0300

    Init:

---
+++ ItemStore.WebApi/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using ItemStore.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ItemStore.WebApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ ItemStore.WebApi/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace ItemStore.WebApi.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; } = 0;
+
+        public bool Delete { get; set; } = false;
+    }
+}

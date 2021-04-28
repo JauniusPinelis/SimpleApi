a66b525032448eb415abb21d6ff765984f083249
JauniusPinelis <JauniusPinelis@gmail.com>
2021-04-28 14:36:56 +0300

    Controller added

---
+++ ItemStore.WebApi/Controllers/ItemController.cs
@@ -0,0 +1,25 @@
+﻿using ItemStore.WebApi.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace ItemStore.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public ItemController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return await _dataContext.Items.ToListAsync();
+        }
+    }
+}
